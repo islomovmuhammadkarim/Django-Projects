d2c3d1a06d6c2751d0dd35ed40602ae12635a496
MuahammadKarim Islomov <muhammadkarim@gmail.com>
2025-11-24 09:29:38 -0600

    Add custom user model with age and address fields, configure authentication, and implement signup view
    - Create CustomUser model extending AbstractUser with age and address fields
    - Add CustomUserAdmin with custom fieldsets for user management
    - Implement signup view using CreateView with CustomUserCreationForm
    - Configure AUTH_USER_MODEL, EMAIL_BACKEND, LOGIN_REDIRECT_URL, and LOGOUT_REDIRECT_URL in settings
    - Add accounts and pages apps to INSTALLED_APPS
    - Configure template directory and include

--- blog2mohirdev/accounts/admin.py
+++ blog2mohirdev/accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm,CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model=CustomUser
+    add_form=CustomUserCreationForm
+    form=CustomUserChangeForm
+    list_display=('username','first_name','last_name','email','age','address','is_staff')
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('username','first_name','last_name','email','age','address','is_staff')
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            "fields": (
+                ('username','first_name','last_name','email','age','address','is_staff')
+            ),
+        }),
+    )
+    
+
+admin.site.register(CustomUser,CustomUserAdmin)
--- blog2mohirdev/accounts/models.py
+++ blog2mohirdev/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    age=models.PositiveBigIntegerField(null=True,blank=True)
+    address=models.CharField(max_length=100,null=True,blank=True)
+    
+    
